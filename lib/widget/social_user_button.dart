9d0f16e2d5345993dd6c34ac79b2292b1ec5c3de
Masamichi Ueta <u_masamichi@icloud.com>
2018-09-20 10:13:00 +0900

    add social user button

---
+++ lib/widget/social_user_button.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
+import '../colors.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+enum SocialType { github, twitter }
+
+class SocialUserButton extends StatelessWidget {
+  final String title;
+  final SocialType type;
+  final String url;
+
+  const SocialUserButton({
+    Key key,
+    this.title,
+    this.type,
+    this.url,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    IconData icon;
+    switch (type) {
+      case SocialType.github:
+        icon = FontAwesomeIcons.githubSquare;
+        break;
+      case SocialType.twitter:
+        icon = FontAwesomeIcons.twitterSquare;
+        break;
+    }
+    return Container(
+        child: FlatButton(
+            onPressed: _launchURL,
+            child: Row(children: [
+              Icon(
+                icon,
+                color: kMtcPrimaryGrey,
+              ),
+              Container(
+                width: 8.0,
+              ),
+              Text(title,
+                  style: TextStyle(
+                      color: kMtcPrimaryGrey,
+                      decoration: TextDecoration.underline))
+            ]),
+            textTheme: ButtonTextTheme.primary));
+  }
+
+  _launchURL() async {
+    if (await canLaunch(url)) {
+      await launch(url);
+    }
+  }
+}
