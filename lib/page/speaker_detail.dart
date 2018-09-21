49ba8a0c6e74228370c57776853c7ed409b1d5ba
Masamichi Ueta <u_masamichi@icloud.com>
2018-09-21 15:47:16 +0900

    localize

--- lib/page/speaker_detail.dart
+++ lib/page/speaker_detail.dart
@@ -8,27 +8,51 @@ class SpeakerDetailPage extends StatelessWidget {
 
   const SpeakerDetailPage({Key key, this.speaker}) : super(key: key);
 
-  Widget buildBody() {
+  Widget buildBody(BuildContext context) {
     return Container(
         color: Colors.white,
         child: ListView(children: [
           Container(
             margin: EdgeInsets.symmetric(vertical: 32.0, horizontal: 24.0),
-            child: buildSpeakerInformation(),
+            child: buildSpeakerInformation(context),
           )
         ]));
   }
 
-  Widget buildSpeakerInformation() {
+  Widget buildSpeakerInformation(BuildContext context) {
+    Locale currentLocale = Localizations.localeOf(context);
+    var profile = currentLocale.languageCode == "ja"
+        ? speaker.profileJa
+        : speaker.profile;
+    var name =
+        currentLocale.languageCode == "ja" ? speaker.nameJa : speaker.name;
+    var position = currentLocale.languageCode == "ja"
+        ? speaker.positionJa
+        : speaker.position;
     return Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: <Widget>[
           Container(
               margin: const EdgeInsets.only(bottom: 16.0),
-              child: buildSessionSpeakerInformation()),
+              child: Container(
+                  child: ListTile(
+                contentPadding: const EdgeInsets.all(0.0),
+                leading: CircleAvatar(
+                  backgroundImage: new NetworkImage(speaker.iconUrl),
+                  radius: 25.0,
+                ),
+                title: Text(name,
+                    style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        color: kMtcPrimaryGrey,
+                        fontSize: 18.0)),
+                subtitle: Text(position,
+                    style: TextStyle(color: kMtcPrimaryGrey, fontSize: 12.0)),
+                // trailing: IconButton(icon: const Icon(Icons.favorite_border), color: Colors.black, onPressed: () { AlertDialog(title: Text('Go to the session!'), content: Text('Hey!')); }),
+              ))),
           Container(
               margin: const EdgeInsets.only(bottom: 24.0),
-              child: Text(speaker.profile,
+              child: Text(profile,
                   style: TextStyle(color: kMtcPrimaryGrey, fontSize: 14.0))),
           buildLinkButtons(speaker)
         ]);
@@ -58,25 +82,6 @@ class SpeakerDetailPage extends StatelessWidget {
             children: [githubLinkButton, twitterLinkButton]));
   }
 
-  Widget buildSessionSpeakerInformation() {
-    return Container(
-        child: ListTile(
-      contentPadding: const EdgeInsets.all(0.0),
-      leading: CircleAvatar(
-        backgroundImage: new NetworkImage(speaker.iconUrl),
-        radius: 25.0,
-      ),
-      title: Text(speaker.name,
-          style: TextStyle(
-              fontWeight: FontWeight.bold,
-              color: kMtcPrimaryGrey,
-              fontSize: 18.0)),
-      subtitle: Text(speaker.position,
-          style: TextStyle(color: kMtcPrimaryGrey, fontSize: 12.0)),
-      // trailing: IconButton(icon: const Icon(Icons.favorite_border), color: Colors.black, onPressed: () { AlertDialog(title: Text('Go to the session!'), content: Text('Hey!')); }),
-    ));
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -84,6 +89,6 @@ class SpeakerDetailPage extends StatelessWidget {
           title: Text(speaker.name),
           centerTitle: false,
         ),
-        body: buildBody());
+        body: buildBody(context));
   }
 }
