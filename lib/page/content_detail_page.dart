29513ad8de8f0e5abd88f61db6f0a3818b5cf37c
masamichiueta <u_masamichi@icloud.com>
2018-10-02 22:55:52 +0900

    update content detail page

--- lib/page/content_detail_page.dart
+++ lib/page/content_detail_page.dart
@@ -22,53 +22,48 @@ class ContentDetailPage extends StatelessWidget {
   Widget buildBody(BuildContext context) {
     Widget image;
     if (exhibition.exhibitionImage != "") {
-      image = ClipRRect(
-          borderRadius: BorderRadius.only(
-              topLeft: Radius.circular(4.0), topRight: Radius.circular(4.0)),
-          child: Image.asset("images/" + exhibition.exhibitionImage));
+      image = Container(
+          padding: EdgeInsets.only(bottom: 24.0),
+          child: ClipRRect(
+              borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(4.0),
+                  topRight: Radius.circular(4.0)),
+              child: Image.asset("images/" + exhibition.exhibitionImage)));
     }
 
     return Container(
-        margin: EdgeInsets.only(top: 24.0),
+        color: Colors.white,
         child: ListView(children: <Widget>[
           Container(
-              padding: const EdgeInsets.only(left: 24.0, right: 24.0),
-              child: Material(
-                  type: MaterialType.canvas,
-                  child: Container(
-                    color: kMtcBackgroundGrey,
-                    child: Container(
-                        margin: const EdgeInsets.only(bottom: 10.0),
-                        child: Text(exhibition.localizedTitle(context),
-                            style: const TextStyle(
-                                fontWeight: FontWeight.bold, fontSize: 22.0))),
-                  ))),
+              color: Colors.white,
+              padding: const EdgeInsets.all(24.0),
+              child: Text(exhibition.localizedTitle(context),
+                  style: const TextStyle(
+                      color: kMtcPrimaryGrey,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 22.0))),
           Container(
-              margin: const EdgeInsets.only(top: 30.0),
-              padding: const EdgeInsets.only(left: 20.0, right: 20.0),
+              padding: const EdgeInsets.only(left: 24.0, right: 24.0),
               color: Colors.white,
               child: Container(
-                  padding:
-                      EdgeInsets.symmetric(vertical: 32.0, horizontal: 0.0),
                   child: Column(
                       crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
-                        image != null ? image : Container(),
-                        Container(
-                            padding: EdgeInsets.fromLTRB(0.0, 24.0, 0.0, 32.0),
-                            child: Text(
-                                exhibition.localizedDescription(context),
-                                style: TextStyle(
-                                    fontSize: 14.0, color: kMtcPrimaryGrey))),
-                        Container(
-                            padding: EdgeInsets.only(bottom: 24.0),
-                            child: Text("BOOTH MAP",
-                                style: TextStyle(
-                                    color: kMtcPrimaryGrey, fontSize: 24.0))),
-                        Container(
-                          child: Image.asset("images/" + exhibition.boothImage),
-                        )
-                      ])))
+                    image != null ? image : Container(),
+                    Container(
+                        padding: EdgeInsets.fromLTRB(0.0, 0.0, 0.0, 32.0),
+                        child: Text(exhibition.localizedDescription(context),
+                            style: TextStyle(
+                                fontSize: 14.0, color: kMtcPrimaryGrey))),
+                    Container(
+                        padding: EdgeInsets.only(bottom: 24.0),
+                        child: Text("BOOTH MAP",
+                            style: TextStyle(
+                                color: kMtcPrimaryGrey, fontSize: 24.0))),
+                    Container(
+                      child: Image.asset("images/" + exhibition.boothImage),
+                    )
+                  ])))
         ]));
   }
 }
